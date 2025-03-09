f76c403d3a0dadfa8308f3cfebd7238280441b58
Robin Linacre <robinlinacre@hotmail.com>
2025-03-09 14:39:59 +0000

    deployed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: './',
     server: {
         port: 3000, // Change this if you want a different dev server port
     },
+    base: '/breakout_maths/',
 });
+
